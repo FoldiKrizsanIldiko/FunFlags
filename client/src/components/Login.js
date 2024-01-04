4be29ce1453ee382835330440be6bb08d4487f63
Surmi64 <surmi64@gmail.com>
2024-01-04 09:31:12 +0100

    [clear] unnecessary props removed from Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
-//import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 
 function Login(props) {
   const setUser = props.setUser;
-  const setScreen = props.setScreen;
   const navigate = useNavigate();
 
   function handleSubmit(event) {
