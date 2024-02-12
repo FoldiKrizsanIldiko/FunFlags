fed536357d1b0260a26dfd4d48a5328d2517cbbb
Mondi18 <vargamondi@gmail.com>
2024-02-12 09:29:26 +0100

    [CHORE]:remove routing,included Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -8,6 +7,7 @@ import ChooseGame from "./components/ChooseGame";
 import QuizMode from "./components/QuizMode";
 import Game from "./components/Game";
 import LeaderBoard from "./components/LeaderBoard";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [data, setData] = useState([]);
@@ -32,57 +32,54 @@ function App() {
       .catch((e) => console.log(e));
   }, []);
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login setUser={setUser} />,
-    },
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Login setUser={setUser} />,
+  //   },
 
-    {
-      path: "/register",
-      element: <Register setUser={setUser} />,
-    },
-
-    {
-      path: "/chooseGameMode",
-      element: <ChooseGame setUser={setUser} />,
-    },
-    {
-      path: "/quiz",
-      element: (
-        <QuizMode data={data} user={user} setSortedUsers={setSortedUsers} />
-      ),
-    },
-    {
-      path: "/game",
-      element: (
-        <div className="gameDiv">
-          <Game
-            data={data}
-            searchBy={searchBy}
-            setSearchBy={setSearchBy}
-            user={user}
-            setSortedUsers={setSortedUsers}
-          />
-        </div>
-      ),
-    },
-    {
-      path: "/leaderboard",
-      element: (
-        <div className="leaderboard">
-          <h1>High scores </h1>
-          <LeaderBoard sortedUsers={sortedUsers} setUser={setUser} />
-        </div>
-      ),
-    },
-  ]);
+  //   {
+  //     path: "/register",
+  //     element: <Register setUser={setUser} />,
+  //   },
+  //   {
+  //     path: "/quiz",
+  //     element: (
+  //       <QuizMode data={data} user={user} setSortedUsers={setSortedUsers} />
+  //     ),
+  //   },
+  //   {
+  //     path: "/game",
+  //     element: (
+  //       <div className="gameDiv">
+  //         <Game
+  //           data={data}
+  //           searchBy={searchBy}
+  //           setSearchBy={setSearchBy}
+  //           user={user}
+  //           setSortedUsers={setSortedUsers}
+  //         />
+  //       </div>
+  //     ),
+  //   },
+  //   {
+  //     path: "/leaderboard",
+  //     element: (
+  //       <div className="leaderboard">
+  //         <h1>High scores </h1>
+  //         <LeaderBoard sortedUsers={sortedUsers} setUser={setUser} />
+  //       </div>
+  //     ),
+  //   },
+  // ]);
   return (
-    <React.StrictMode>
-      <div className="App">
-        <RouterProvider router={router} />
-      </div>
-    </React.StrictMode>
+    <div className="App">
+      {user ? (<QuizMode data={data} user={user} setSortedUsers={setSortedUsers} />) :
+        (<Login setUser={setUser} />)
+      }
+      {/* <Outlet/> */}
+    </div>
+      
   );
 }
 
