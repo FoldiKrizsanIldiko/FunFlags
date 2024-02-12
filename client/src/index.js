de61f36d4407d427c90ecb41def0ae7dd6074f48
Mondi18 <vargamondi@gmail.com>
2024-02-12 09:26:25 +0100

    [FEAT]: include routing

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,41 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Login from "./components/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import QuizMode from "./components/QuizMode";
+import LeaderBoard from "./components/LeaderBoard";
+import Register from "./components/Register";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+            path:"/quiz",
+            element:<QuizMode/>
+        },
+        {
+            path:"/leaderboard",
+            element:<LeaderBoard/>
+        },
+        // {
+        //   path:"/register",
+        //   element:<Register/>
+        // }
+      ],
+    },
+  ]);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 reportWebVitals();
