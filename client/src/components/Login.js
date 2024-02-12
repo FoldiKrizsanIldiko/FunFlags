9ad766ec71732ae42b8d08566800f39ab5ca6afa
Mondi18 <vargamondi@gmail.com>
2024-02-12 09:27:06 +0100

    [CHORE]:create new form for user

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,106 +1,48 @@
-import React,{ useEffect } from "react";
+import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import {
-  loadCaptchaEnginge,
-  LoadCanvasTemplate,
-  validateCaptcha,
-} from "react-simple-captcha";
-
+import "../styles/Login.css"
 function Login(props) {
   const setUser = props.setUser;
   const navigate = useNavigate();
 
-  useEffect(() => {
-    loadCaptchaEnginge(6);
-  }, []);
-
   function handleSubmit(event) {
     event.preventDefault();
- submitCaptcha()&& 
+    console.log(event)
     fetch(
-      `https://zldnuw6vi1.execute-api.eu-west-2.amazonaws.com/user/${event.target[0].value}/${event.target[1].value}`
+      `https://4rjjlsl47e.execute-api.eu-west-2.amazonaws.com/flags-get1/user/${event.target[0].value}/${event.target[1].value}`
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         return data.name
-          ? (setUser(data), navigate("/chooseGameMode"))
+          ? (setUser(data), navigate("/quiz"))
           : toast.error("Incorrect name and/or password!", {
-              theme: "colored",
-              position: "top-right",
-            });
+            theme: "colored",
+            position: "top-right",
+          });
       });
   }
-
-   function submitCaptcha() {
-     let user_captcha = document.getElementById("user_captcha_input").value;
-
-     if (validateCaptcha(user_captcha) === true) {
-       return true;
-     } else {
-       alert("Captcha Does Not Match");
-       document.getElementById("user_captcha_input").value = "";
-       return false;
-     }
-   }
-
+  
   return (
-    <div className="App">
-      <div className="login">
-        <h1> Welcome</h1>
-        <h2>
-          This is an educational game. <br />
-          You have to guess which country's flag is displayed.
-          <br />
-          Before you start, please log in:
-        </h2>
-        <br></br>
-        <form className="userForm" onSubmit={(event) => handleSubmit(event)}>
-          <div className="user-box">
-            <input type="text" required={true}></input>
-            <label>Name: </label>
-          </div>
-          <div className="user-box">
-            <input type="password" required={true}></input>
-            <label>Password: </label>
-          </div>
-    <ToastContainer theme="dark" position="top-right" />
-        <div className="container">
-          <div className="form-group">
-            <div className="col mt-3">
-              <LoadCanvasTemplate />
-            </div>
+    <div className="bg">
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <h3>Login Here</h3>
 
-            <div className="col mt-3">
-              <div>
-                <input
-                  placeholder="Enter Captcha Value"
-                  id="user_captcha_input"
-                  name="user_captcha_input"
-                  type="text"
-                >
-                </input>
-              </div>
-            </div>
-          </div>
+        <label htmlFor="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" required={true} />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" id="password" required={true} />
+
+        <button >Log In</button>
+        <div className="social">
+          <div className="go" onClick={()=>navigate("/register")}>Register now </div>
         </div>
-          
-          <button type="submit" className="userFormButton">
-            Start game<span></span>
-          </button>
-        </form>
-<p>We temporarily removed registration.</p>
-{/*<button
-          style={{ fontSize: "small" }}
-          onClick={() => submitCaptcha() && navigate("/register")}
-          className="userFormButton"
-        >
-          Register<span></span>
-        </button>*/}
-    
-      </div>
+      </form>
+
     </div>
+
   );
 }
 export default Login;
