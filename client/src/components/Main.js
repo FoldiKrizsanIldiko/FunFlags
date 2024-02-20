fa422564dda7bafb15f0a33ec61f8fa3ef5abe86
Mondi18 <vargamondi@gmail.com>
2024-02-20 21:19:28 +0100

    [FEAT]:conditional rendering for Login page, included Outlet,useContext

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect,createContext } from "react";
 import Login from "./Login";
 import QuizMode from "./QuizMode";
 import { Outlet } from "react-router-dom";
-
+import App from "../App";
+export const UserContext=createContext(null);
+export const RestCountriesContext=createContext([]);
 function Main() {
   const [data, setData] = useState([]);
   const [searchBy, setSearchBy] = useState("");
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const [sortedUsers, setSortedUsers] = useState([]);
+  
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -70,11 +72,18 @@ function Main() {
   // ]);
   return (
     <div>
-      <Login setUser={setUser} />,
-      <QuizMode data={data} user={user} setSortedUsers={setSortedUsers} />
+      <RestCountriesContext.Provider value={{data,setData}}>
+      <UserContext.Provider value={{user,setUser}}>
+      <QuizMode setSortedUsers={setSortedUsers} />
+      {!user? <Login/>:
+      <>
+      <App/>
       <Outlet/>
+      </>
+      }
+    </UserContext.Provider>
+    </RestCountriesContext.Provider>
     </div>
-      
   );
 }
 
