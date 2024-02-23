99bb463f596e647f47b3a8e3280d9d6a41c3dea2
Surmi64 <surmi64@gmail.com>
2024-02-23 03:43:32 +0100

    [update] App renamed to Sidebar, animations added

--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,49 @@
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Login from "./components/Login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import QuizMode from "./components/QuizMode";
 import LeaderBoard from "./components/LeaderBoard";
-import Register from "./components/Register";
+import Login from "./components/Login";
 import Main from "./components/Main";
+import QuizMode from "./components/QuizMode";
+import Register from "./components/Register";
+import Sidebar from "./components/Sidebar";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children: [
-        {
-          path: "/login",
-          element: <Login />,
-        },
-        {
-            path:"/quiz",
-            element:<QuizMode/>
-        },
-        {
-            path:"/leaderboard",
-            element:<LeaderBoard/>
-        },
-        {
-          path:"/register",
-          element:<Register/>
-        },  
-        {
-          path:"/side",
-          element:<App/>
-        }
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/quiz",
+        element: <QuizMode />,
+      },
+      {
+        path: "/leaderboard",
+        element: <LeaderBoard />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/side",
+        element: <Sidebar />,
+      },
+    ],
+  },
+]);
 
-      ],
-    },
-  ]);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
 reportWebVitals();
