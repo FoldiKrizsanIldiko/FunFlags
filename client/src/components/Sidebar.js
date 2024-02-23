b106e25fbacbb541007e5af3f9452ecbe146b830
Surmi64 <surmi64@gmail.com>
2024-02-23 03:42:56 +0100

    [update] App renamed to Sidebar, animations added

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import "./styles/App.css";
+import "../styles/Sidebar.css";
+import React, { useState, useContext } from "react";
+import { UserContext } from "./Main";
 
-function App() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
+function Sidebar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { user, setUser } = useContext(UserContext);
 
   const handleOpen = () => {
     setIsDrawerOpen(!isDrawerOpen);
@@ -17,24 +19,29 @@ function App() {
           className={isDrawerOpen === true ? "open" : "closed"}
           onClick={handleOpen}
         >
-          {isDrawerOpen == true ? (
+          {isDrawerOpen === true ? (
             <i className="fa-classic fa-solid fa-chevron-left"></i>
           ) : (
             <i className="fa-classic fa-solid fa-chevron-right"></i>
           )}
         </div>
       </div>
-      <div className={isDrawerOpen === true ? "" : "sidebar-hidden"}>
+      <div
+        className={isDrawerOpen === true ? "open-user-icon" : "sidebar-hidden"}
+      >
         <i className="fa-solid fa-user"></i>
+        <span>{user.name}</span>
       </div>
       <div className={isDrawerOpen === true ? "btn" : "sidebar-hidden"}>
         <button>Game Modes</button>
         <button>Settings</button>
         <button>High Scores</button>
-        <button className="logout">Logout</button>
+        <button className="logout" onClick={() => setUser(null)}>
+          Logout
+        </button>
       </div>
     </div>
   );
 }
 
-export default App;
+export default Sidebar;
