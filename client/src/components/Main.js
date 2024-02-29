6e7da43a622d9c806df77567e3cab5a086fb84a0
Mondi18 <vargamondi@gmail.com>
2024-02-29 18:50:38 +0100

    [FEAT]:included Leaderboard, add to ternary

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,6 +7,8 @@ import { Outlet } from "react-router-dom";
 import { useState, createContext } from "react";
 import Register from "./Register"
 import { useLocation } from 'react-router-dom';
+import LeaderBoard from "./LeaderBoard";
+
 
 export const UserContext = createContext(null);
 export const RestCountriesContext = createContext([]);
@@ -16,19 +18,23 @@ function Main() {
   const [data, setData] = useState([]);
   const [user, setUser] = useState(null);
   const isRegisterRoute = location.pathname === '/register';
- 
+  const isLeaderBoardRoute=location.pathname==="/leaderboard";
+
   return (
     <div>
       <RestCountriesContext.Provider value={{ data, setData }}>
         <UserContext.Provider value={{ user, setUser }}>
-          <QuizMode />
+           <QuizMode />
           {!user ? (
             !isRegisterRoute ? <Login /> : <Register />
           ) : (
+              !isLeaderBoardRoute?
             <div className="game-area">
               <Sidebar />
               <Outlet />
             </div>
+            :
+            <LeaderBoard/>
           )}
         </UserContext.Provider>
       </RestCountriesContext.Provider>
@@ -37,35 +43,3 @@ function Main() {
 }
 
 export default Main;
-// function App() {
-//   const [user, setUser] = useState(null);
-//   const [showRegister, setShowRegister] = useState(false); // State variable to track if Register should be displayed
-
-//   return (
-//     <div>
-//       <RestCountriesContext.Provider value={{ data, setData }}>
-//         <UserContext.Provider value={{ user, setUser }}>
-//           <QuizMode />
-//           {!user && !showRegister ? ( // Display Login if user is not logged in and showRegister is false
-//             <Login onRegisterClick={() => setShowRegister(true)} /> // Pass a callback to handle the register button click
-//           ) : (
-//             <div className="game-area">
-//               <Sidebar />
-//               <Outlet />
-//             </div>
-//           )}
-//           {showRegister && <Register />} {/* Display Register component if showRegister is true */}
-//         </UserContext.Provider>
-//       </RestCountriesContext.Provider>
-//     </div>
-//   );
-// }
-
-// export default App;
-// function Login({ onRegisterClick }) {
-//   const handleRegisterClick = () => {
-//     // Call the onRegisterClick callback passed from the parent component
-//     if (typeof onRegisterClick === 'function') {
-//       onRegisterClick();
-//     }
-//   };
